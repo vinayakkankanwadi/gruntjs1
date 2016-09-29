97d9afbe48dadc93b21722c0153b0f648ebb83b8
Vinayak <vinayakkankanwadi@hotmail.com>
2016-09-29 22:52:10 +1000

    Update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,3 +2,8 @@ var grunt = require('grunt');
 grunt.registerTask('default', 'default task description', function(){
   console.log('hello world');
 });
+grunt.registerTask('hello', 'say hello', function(name){
+  if(!name || !name.length)
+    grunt.warn('you need to provide a name.');
+  console.log('hello ' + name);
+});
